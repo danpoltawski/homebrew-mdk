1e122c4c9bdcd97c7f512f2f7ff86d56fbcd051c
Dan Poltawski <dan@moodle.com>
2013-02-08 16:38:23 +0800

    Use correct dir and rename to moodle-sdk

--- Formula/moodle-sdk.rb
+++ Formula/moodle-sdk.rb
@@ -3,17 +3,20 @@ require 'formula'
 # Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-class MoodleMdk < Formula
+class MoodleSdk < Formula
   homepage 'https://github.com/FMCorz/mdk/'
   url 'https://github.com/FMCorz/mdk/archive/master.zip'
-  sha1 '84166bbe2c12bbea6481451cdc96850a8169b08d'
+  sha1 '399c603370817d15042f9e4f8f278d25e4dbc6fc'
   version '0.1'
 
   def install
-      libexec.install 'moodle'
-      libexec.install Dir['moodle-*.py']
-      libexec.install Dir['lib']
-      bin.install_symlink libexec+'moodle' => 'mdk'
+      dir = (libexec+'moodle-sdk')
+      dir.mkpath
+      dir.install 'moodle'
+      dir.install 'config-dist.json'
+      dir.install Dir['moodle-*.py']
+      dir.install Dir['lib']
+      bin.install_symlink dir+'moodle' => 'mdk'
       bash_completion.install  'extra/bash_completion'
   end
 end
