ddec32e50c20f51265b6fc7a1dd42302f17b633a
Dan Poltawski <dan@moodle.com>
2013-07-16 09:51:06 +0800

    0.4.1 change should have 0.4.1 version number!

--- Formula/moodle-sdk.rb
+++ Formula/moodle-sdk.rb
@@ -7,7 +7,7 @@ class MoodleSdk < Formula
   homepage 'https://github.com/FMCorz/mdk/'
   url 'https://github.com/FMCorz/mdk/archive/v0.4.1.zip'
   sha1 '4e53ec5e1942c7d902a8889e258dc67acb841c45'
-  version '0.4'
+  version '0.4.1'
 
   def install
       dir = (libexec+'moodle-sdk')
