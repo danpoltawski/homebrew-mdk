8fe7cb2d86ac5ab2a8591a9c316a8eb1007be710
Jun Pataleta <jun@moodle.com>
2021-11-02 10:30:40 +0800

    Bump to v2.0.4

--- Formula/moodle-sdk.rb
+++ Formula/moodle-sdk.rb
@@ -5,9 +5,9 @@ require 'formula'
 
 class MoodleSdk < Formula
   homepage 'https://github.com/FMCorz/mdk/'
-  url 'https://github.com/FMCorz/mdk/archive/v2.0.3.zip'
-  sha256 '315a462e883a363899973cd2b2878a825cc20723ae507bb64e09e4ec0c3a0e38'
-  version '2.0.3'
+  url 'https://github.com/FMCorz/mdk/archive/v2.0.4.zip'
+  sha256 'c0d1db905ceff9469b7477a5b2ca8d21594968640941e014a34374e59cc62f11'
+  version '2.0.4'
 
   def install
       dir = (libexec+'moodle-sdk')
